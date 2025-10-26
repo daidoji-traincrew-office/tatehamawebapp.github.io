f06f484ed6ce8f86bad93ebaf17212b4329bbaa4
TakumiteTudo <takumit2886@gmail.com>
2025-10-26 16:16:59 +0900

    PD自動切換

--- tatehamawebapp/receivedata.js
+++ tatehamawebapp/receivedata.js
@@ -1,12 +1,24 @@
 // JavaScript source code
 async function Getdata() {
-    const requestURL = "https://traincrew-multiats-server-passenger.kesigomon.com/api/train"
-    //const requestURL = "https://traincrew-multiats-server-passenger-dev.kesigomon.com/api/train"
+    const requestURL_P = "https://traincrew-multiats-server-passenger.kesigomon.com/api/train"
+    const requestURL_D = "https://traincrew-multiats-server-passenger-dev.kesigomon.com/api/train"
 
-    const reqest = new Request(requestURL);
+    // Prod
+    const reqest_P = new Request(requestURL_P);
+    const responce_P = await fetch(reqest_P);
+    const datastring_P = await responce_P.text();
+    Location_data_P = JSON.parse(datastring_P);
 
-    const responce = await fetch(reqest);
-	const datastring = await responce.text(); // レスポンスをテキストとして取得
+    // Dev
+    const reqest_D = new Request(requestURL_D);
+    const responce_D = await fetch(reqest_D);
+    const datastring_D = await responce_D.text();
+    Location_data_D = JSON.parse(datastring_D);
 
-	Location_data = JSON.parse(datastring);
+    if (Location_data_D.ServerMode) {
+        Location_data = Location_data_D;
+    }
+    else if (Location_data_P.ServerMode) {
+        Location_data = Location_data_P;
+    }
 }
